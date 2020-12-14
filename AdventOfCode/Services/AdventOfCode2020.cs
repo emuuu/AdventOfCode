92cb40b8e42452c7ede0587fa5f5dbbc65ea98e5
Timm <tb@fm-gruppe.com>
2020-12-14 12:49:40 +0100

    Removed unused extension

--- AdventOfCode/Services/AdventOfCode2020.cs
+++ AdventOfCode/Services/AdventOfCode2020.cs
@@ -1037,14 +1037,4 @@ namespace AdventOfCode.Services
         }
         #endregion
     }
-
-    public static class IEnumerablerExtension
-    {
-        public static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(this IEnumerable<T> elements, int k)
-        {
-            return k == 0 ? new[] { new T[0] } :
-              elements.SelectMany((e, i) =>
-                elements.Skip(i + 1).DifferentCombinations(k - 1).Select(c => (new[] { e }).Concat(c)));
-        }
-    }
 }
