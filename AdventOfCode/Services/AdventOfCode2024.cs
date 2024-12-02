f4fc17324581b8ea20d8b2aca876779a883cedac
Timm Bremekamp <tb@fm-gruppe.com>
2024-12-02 07:32:18 +0100

    added 2024

---
+++ AdventOfCode/Services/AdventOfCode2024.cs
@@ -0,0 +1,64 @@
+﻿using AdventOfCode.Extensions;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Services
+{
+    public class AdventOfCode2024
+    {
+        public AdventOfCode2024()
+        {
+
+        }
+
+        [MethodInList]
+        public List<string> SolveDay(string inputPath, int day)
+        {
+            var type = this.GetType();
+            var inputMethod = type.GetMethod("ConvertDay" + day + "Input");
+            var puzzle1Method = type.GetMethod("Day" + day + "_PuzzleOne");
+            var puzzle2Method = type.GetMethod("Day" + day + "_PuzzleTwo");
+
+            var result = new List<string>() { "Day " + day };
+            var watch = new Stopwatch();
+            if (inputMethod != null)
+            {
+                if (puzzle1Method != null)
+                {
+                    var input = inputMethod.Invoke(this, new object[] { inputPath });
+                    watch.Start();
+                    var puzzleResult = puzzle1Method.Invoke(this, new object[] { input });
+                    watch.Stop();
+
+                    result.Add($"Puzzle 1: {puzzleResult} ({watch.ElapsedMilliseconds} ms)");
+                }
+                else
+                {
+                    result.Add("Puzzle 1: not implemented");
+                }
+                if (puzzle2Method != null)
+                {
+                    var input = inputMethod.Invoke(this, new object[] { inputPath });
+                    watch.Restart();
+                    var puzzleResult = puzzle2Method.Invoke(this, new object[] { input });
+                    watch.Stop();
+                    result.Add($"Puzzle 2: {puzzleResult} ({watch.ElapsedMilliseconds} ms)");
+                }
+                else
+                {
+                    result.Add("Puzzle 2: not implemented");
+                }
+            }
+            else
+            {
+                result.Add("Not implemented");
+            }
+            return result;
+        }
+    }
+}
+
